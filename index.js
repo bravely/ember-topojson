019beb152ac874fa07dff91549f7c8652612acb4
Jake Mays <jake.m.mays@gmail.com>
2016-04-28 12:28:27 -0600

    Typo.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = {
     this._super.included(app);
 
     app.import(app.bowerDirectory + '/topojson/topojson.js');
-    app.import('vendor/ember-topojson-shim.js', {
+    app.import('vendor/ember-topojson/ember-topojson-shim.js', {
       exports: {
         topojson: ['default']
       }
